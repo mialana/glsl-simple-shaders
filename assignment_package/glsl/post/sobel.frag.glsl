d2446146931f01f9593575d1eca0295b77307038
mialana <96770662+mialana@users.noreply.github.com>
2023-02-16 23:52:18 -0500

    finished sobel shader and noise shader

--- assignment_package/glsl/post/sobel.frag.glsl
+++ assignment_package/glsl/post/sobel.frag.glsl
@@ -8,8 +8,35 @@ uniform sampler2D u_RenderedTexture;
 uniform int u_Time;
 uniform ivec2 u_Dimensions;
 
+const mat3 horizontal = mat3(3.f, 0.f, -3.f,
+                             10.f, 0.f, -10.f,
+                             3.f, 0.f, -3.f);
+
+const mat3 vertical = mat3(3.f, 10.f, 3.f,
+                           0.f, 0.f, 0.f,
+                           -3.f, -10.f, -3.f);
+
 void main()
 {
-    // TODO Homework 5
-    color = vec3(0, 0, 0);
+    vec2 pixelUV = 1.f / u_Dimensions;
+
+    vec3 horGrad = vec3(0, 0, 0);
+    vec3 vertGrad = vec3(0, 0, 0);
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            int xDist = i - 1;
+            int yDist = j - 1;
+            vec2 texCoord = fs_UV + vec2(float(xDist) * pixelUV.x, float(yDist) * pixelUV.y);
+            vec3 color = vec3(texture(u_RenderedTexture, texCoord));
+            horGrad += horizontal[i][j] * color;
+            vertGrad += vertical[i][j] * color;
+        }
+    }
+
+    float x = distance(horGrad.x, vertGrad.x);
+    float y = distance(horGrad.y, vertGrad.y);
+    float z = distance(horGrad.z, vertGrad.z);
+
+    color = sqrt((horGrad * horGrad) + (vertGrad * vertGrad));
 }
--- assignment_package/glsl/post/worleywarp.frag.glsl
+++ assignment_package/glsl/post/worleywarp.frag.glsl
@@ -9,8 +9,114 @@ out vec3 color;
 
 uniform sampler2D u_RenderedTexture;
 
+vec2 random2( vec2 p ) {
+    return fract(sin(vec2(dot(p, vec2(127.1, 311.7)),
+                 dot(p, vec2(269.5,183.3))))
+                 * 43758.5453);
+}
+
+float worleyNoise(vec2 uv) {
+    uv *= 10.0; // Now the space is 10x10 instead of 1x1. Change this to any number you want.
+    vec2 uvInt = floor(uv);
+    vec2 uvFract = fract(uv);
+    float minDist = 1.0; // Minimum distance initialized to max.
+    for(int y = -1; y <= 1; ++y) {
+        for(int x = -1; x <= 1; ++x) {
+            vec2 neighbor = vec2(float(x), float(y)); // Direction in which neighbor cell lies
+            vec2 point = random2(uvInt + neighbor); // Get the Voronoi centerpoint for the neighboring cell
+            vec2 diff = neighbor + point - uvFract; // Distance between fragment coord and neighbor’s Voronoi point
+            float dist = length(diff);
+            minDist = min(minDist, dist);
+        }
+    }
+    return minDist;
+}
+
+
+float surflet(vec2 P, vec2 gridPoint) {
+    // Compute falloff function by converting linear distance to a polynomial
+    float distX = abs(P.x - gridPoint.x);
+    float distY = abs(P.y - gridPoint.y);
+    float tX = 1 - 6 * pow(distX, 5.f) + 15 * pow(distX, 4.f) - 10 * pow(distX, 3.f);
+    float tY = 1 - 6 * pow(distY, 5.f) + 15 * pow(distY, 4.f) - 10 * pow(distY, 3.f);
+    // Get the random vector for the grid point
+    vec2 gradient = 2.f * random2(gridPoint) - vec2(1.f);
+    // Get the vector from the grid point to P
+    vec2 diff = P - gridPoint;
+    // Get the value of our height field by dotting grid->P with our gradient
+    float height = dot(diff, gradient);
+    // Scale our height field (i.e. reduce it) by our polynomial falloff function
+    return height * tX * tY;
+}
+
+float perlinNoise(vec2 uv) {
+        float surfletSum = 0.f;
+        // Iterate over the four integer corners surrounding uv
+        for(int dx = 0; dx <= 1; ++dx) {
+                for(int dy = 0; dy <= 1; ++dy) {
+                        surfletSum += surflet(uv, floor(uv) + vec2(dx, dy));
+                }
+        }
+        return surfletSum;
+}
+
+float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+
+float noise(in vec3 p) {
+    vec3 a = floor(p);
+    vec3 d = p - a;
+    d = d * d * (3.0 - 2.0 * d);
+
+    vec4 b = vec4(a.x, a.x, a.y, a.y) + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 k1 = perm(b.xyxy);
+    vec4 k2 = perm(k1.xyxy + b.zzww);
+
+    vec4 c = k2 + vec4(a.z, a.z, a.z, a.z);
+    vec4 k3 = perm(c);
+    vec4 k4 = perm(c + 1.0);
+
+    vec4 o1 = fract(k3 * (1.0 / 41.0));
+    vec4 o2 = fract(k4 * (1.0 / 41.0));
+
+    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+
+    return o4.y * d.y + o4.x * (1.0 - d.y);
+}
+
+float fbm(vec3 x) {
+        float v = 0.0;
+        float a = 0.5;
+        vec3 shift = vec3(100);
+        for (int i = 0; i < 8; ++i) {
+                v += a * noise(x);
+                x = x * 2.f + shift;
+                a *= 0.5;
+        }
+        return v;
+
+}
+
 void main()
 {
-    // TODO Homework 5
-    color = vec3(0, 0, 0);
+    float t = sin(u_Time * 0.03f);
+
+    float worley = worleyNoise(fs_UV);
+
+    float perlin = perlinNoise(fs_UV);
+
+    float red = 0.5f + 0.5f * cos(2.f * radians(180.f) * (t * perlin + 0.5f));
+    float green = 0.5f + 0.5f * cos(2.f * radians(180.f) * (t * perlin + 0.7f));
+    float blue = 0.5f + 0.5f * cos(2.f * radians(180.f) * (t * perlin + 0.9f));
+
+    float t2 = sin(u_Time * 0.08f) * worley;
+
+    float uvMixedX = mix(fs_UV.x, fs_UV.x + 0.1f * (sin(worley) * 0.05f + .5f), -t2);
+    float uvMixedY = mix(fs_UV.y, fs_UV.y - 0.2f * (sin(worley) * 0.05f - .5f), -t2);
+
+    vec4 diffuseColor = texture(u_RenderedTexture, vec2(uvMixedX, uvMixedY));
+
+    color = vec3(diffuseColor.rgb * worley / vec3(red, green, blue));
 }
